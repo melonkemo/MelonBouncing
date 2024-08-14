6f17bcbab35231ca4c796d35ff90c13e733462c4
MelonDev <melondev@icloud.com>
2024-08-15 00:28:16 +0700

    Clear unused file

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <melon_bouncing/melon_bouncing_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  MelonBouncingPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MelonBouncingPluginCApi"));
+}
